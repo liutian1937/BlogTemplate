b0b0882856d730bf62908f3ee23220d5e9441b45
liutian1937 <ok8008@yeah.net>
2013-05-21 01:30:26 +0800

    add

---
+++ js/common.js
@@ -0,0 +1,25 @@
+﻿/*
+	Name: Javascript Common Function
+	Link: niumowang.org
+	Author: ok8008@yeah.net
+*/
+function addClass(element,value){
+	if(!element.className){
+		element.className = value;
+	}else{
+		var oValue = element.className;
+		oValue += " ";
+		oValue += value;
+		element.className = oValue;
+	}
+};
+function removeClass(element,value){
+	if (hasClass(element,value)){
+		var reg = new RegExp('(\\s|^)' + value + '(\\s|$)');
+		element.className = element.className.replace(reg, ' ');
+	}
+};
+function hasClass(element,value){
+	var reg = new RegExp('(\\s|^)' + value + '(\\s|$)');
+	return element.className.match(reg);
+}
